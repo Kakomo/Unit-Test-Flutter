b5e533ef4a9fa0d58784fbb02d1bf4241920882b
Kakomo <kakomo2@gmail.com>
2020-11-06 20:53:13 -0300

    Class 2

--- test/dashboard_widget_test.dart
+++ test/dashboard_widget_test.dart
@@ -2,23 +2,32 @@ import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void main(){
-  testWidgets('Show_One_Image_On_Dashboard', (WidgetTester tester) async{
-   await tester.pumpWidget(MaterialApp(home: Dashboard()));
+void main() {
+  testWidgets('Show_One_Image_On_Dashboard', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: Dashboard()));
     final mainImage = find.byType(Image);
     expect(mainImage, findsOneWidget);
   });
-  testWidgets('Show_FeatureItems_On_Dashboard', (tester) async{
+  testWidgets('Show_Transfer_FeatureItem_On_Dashboard', (tester) async {
     await tester.pumpWidget(MaterialApp(home: Dashboard()));
-    final featureItemWidget = find.byType(FeatureItem);
-    expect(featureItemWidget, findsWidgets);
+    final transferFeatureItem = find.byWidgetPredicate((widget) {
+      return featureItemMatcher(widget, 'Transfer', Icons.monetization_on);
+    });
+    expect(transferFeatureItem, findsOneWidget);
   });
-
+  testWidgets('Show_TransactionFeed_FeatureItem_On_Dashboard', (tester) async {
+    await tester.pumpWidget(MaterialApp(home: Dashboard()));
+    final transactionFeedFeatureItem = find.byWidgetPredicate((widget) {
+   return featureItemMatcher(widget, 'Transaction Feed', Icons.description);
+    });
+    expect(transactionFeedFeatureItem, findsOneWidget);
+        });
 }
 
-
-
-
-
-
-
+bool featureItemMatcher(Widget widget, String name, IconData icon) {
+  if (widget is FeatureItem) {
+    return widget.name == name &&
+        widget.icon == icon;
+  }
+  return false;
+}
